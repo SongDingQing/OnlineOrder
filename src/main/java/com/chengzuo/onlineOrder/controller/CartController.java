d64773969e89ca2079e46a93ed9229eda86dfa1c
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2022-10-06 17:05:46 -0400

    1.0

---
+++ src/main/java/com/chengzuo/onlineOrder/controller/CartController.java
@@ -0,0 +1,23 @@
+package com.chengzuo.onlineOrder.controller;
+
+import com.chengzuo.onlineOrder.entity.Cart;
+import com.chengzuo.onlineOrder.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @RequestMapping(value = "/cart", method = RequestMethod.GET)
+    @ResponseBody
+    public Cart getCart(){
+        return cartService.getCart();
+    }
+}
+
---
+++ src/main/java/com/chengzuo/onlineOrder/service/MenuInfoService.java
@@ -0,0 +1,29 @@
+package com.chengzuo.onlineOrder.service;
+import com.chengzuo.onlineOrder.dao.MenuInfoDao;
+import com.chengzuo.onlineOrder.entity.MenuItem;
+import com.chengzuo.onlineOrder.entity.Restaurant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuInfoService {
+
+    @Autowired
+    private MenuInfoDao menuInfoDao;
+
+
+    public List<Restaurant> getRestaurants() {
+        return menuInfoDao.getRestaurants();
+    }
+
+    public List<MenuItem> getAllMenuItem(int restaurantId) {
+        return menuInfoDao.getAllMenuItem(restaurantId);
+    }
+
+    public MenuItem getMenuItem(int id) {
+        return menuInfoDao.getMenuItem(id);
+    }
+}
+
